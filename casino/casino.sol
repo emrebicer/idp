98bbeaeff88a10e3a84e12c7a03fa36be5b6fd4d
Emre Biçer <emre.bicer@motius.de>
2023-12-16 13:04:36 +0100

    add DAI option

--- casino/casino.sol
+++ casino/casino.sol
@@ -5,14 +5,16 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Gambling {
 
     IERC20 public USDC;
+    IERC20 public DAI;
 
     address public owner;
 
-    constructor(address USDCAddres) {
+    constructor(address USDCAddres, address DAIAddress) {
         // Set the owner to the creator of this contract
         owner = msg.sender;
 
         USDC = IERC20(USDCAddres);
+        DAI = IERC20(DAIAddress);
     }
 
     function betUSDC(uint amount) public {
@@ -58,6 +60,52 @@ contract Gambling {
         USDC.transferFrom(address(this), msg.sender, amount);
     }
 
+
+    function betDAI(uint amount) public {
+        // Get the received amount
+        uint reward = amount * 2;
+
+        // Make sure there is a positive bet
+        require(amount > 0, "there has to be a positive bet amount");
+
+        // Make sure we have enough funds
+        require(getDAI() >= reward, "there is not enough DAI for this bet");
+
+        // Collect the bet amount
+        DAI.transferFrom(msg.sender, address(this), amount);
+
+        // Get random value to see if the gambler wins
+        uint random = generateRandomNumber(100);
+
+        if (random > 50) {
+            // User loses, 
+            emit gamblerLostDAI(msg.sender, amount);
+        } else {
+            // User wins
+            emit gamblerWonDAI(msg.sender, reward);
+            DAI.transferFrom(address(this), msg.sender, reward);
+        }
+    }
+
+    function getDAIBalance() private view returns (uint) {
+        return DAI.balanceOf(address(this));
+    }
+
+    function depositDAIFunds(uint amount) public {
+        require(amount > 0, "you must specify a positive DAI value to deposit");
+
+        DAI.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdrawDAIFunds(uint amount) public {
+        require(msg.sender == owner, "only the owner can withdraw funds");
+        require(getDAIBalance() >= amount, "there is not enough DAI funds for the given amount");
+
+        DAI.transferFrom(address(this), msg.sender, amount);
+    }
+
+
+
     // Generate an integer between 0 and upperLimit
     function generateRandomNumber(uint upperLimit) private returns (uint) {
 
@@ -65,8 +113,8 @@ contract Gambling {
     }
 
     // Event definitions for logs
-    event gamblerLostDaiToken(address gambler, uint amount);
-    event gamblerWonDaiToken(address gambler, uint reward);
+    event gamblerLostDAI(address gambler, uint amount);
+    event gamblerWonDAI(address gambler, uint reward);
 
     event gamblerLostUSDC(address gambler, uint amount);
     event gamblerWonUSDC(address gambler, uint reward);
