1fba355663828dd7395db238b3df3ce3bf087aa2
Emre Bicer <oebicer@gmail.com>
2024-01-19 00:02:13 +0100

    deploy erc20 to sepolia

--- us5-ethereum-contract/deployments/deploy.js
+++ us5-ethereum-contract/deployments/deploy.js
@@ -1,4 +1,5 @@
-// Deployed at 0x308516B866620fe50e1Af6EdD895C3b823711466
+// TokenSender deployed at 0x308516B866620fe50e1Af6EdD895C3b823711466
+// MyERC20Token deployed at 0xD82dA44EC61340739270D0116060E91eDf19168d
 async function main() {
   const HelloWorld = await ethers.getContractFactory("TokenSender");
   const hello_world = await HelloWorld.deploy();
