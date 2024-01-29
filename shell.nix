d0abbfe90061e826cf065a28cb942cf98af8aab5
Emre Biçer <oebicer@gmail.com>
2024-01-29 17:10:30 +0100

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [ 
+      yarn
       nodejs_20
       #solc-select
       solc
+      nodePackages_latest.typescript-language-server
     ];
 }
